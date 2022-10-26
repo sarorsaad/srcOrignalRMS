e89c03e7fbf95b5fe18e0134ae06a0196ba01a21
elsaad92 <99730108+elsaad92@users.noreply.github.com>
2022-10-26 12:56:19 +0300

    added new field to pt class

--- hospital/forms.py
+++ hospital/forms.py
@@ -44,7 +44,7 @@ class PatientForm(forms.ModelForm):
     assignedDoctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Name and Department", to_field_name="user_id")
     class Meta:
         model=models.Patient
-        fields=['address','mobile','status','symptoms','profile_pic']
+        fields='__all__'
 
 
 
